1ef59ed5071da43fd2542ff542265b76239bece7
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-08-02 14:50:41 +0700

    Add function for adding books to the library

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
+const myLibrary = [];
+
 function Book(pages, author, year) {
     this.id = crypto.randomUUID();
     this.pages = pages;
     this.author = author;
     this.year = year;
 }
+
+function addBookToLibrary(pages, author, year) {
+    const book = new Book(pages, author, year); 
+    myLibrary.push(book);
+}
+
+addBookToLibrary(121, "John", 1999);
+addBookToLibrary(99, "Johnson", 2001);
+console.log(myLibrary);
