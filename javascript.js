538d1ac9215a483178f875aa1c660981840b6b8c
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-08-04 14:08:27 +0700

    Change buttons' text to svg icons

--- javascript.js
+++ javascript.js
@@ -66,20 +66,25 @@ function showBooks() {
         readText.textContent = "Read: ";
         readStatus.append(readText, item.read ? "Yes" : "No");
 
+        // Buttons
+        const btnGroup = document.createElement("div");
+        btnGroup.classList.add("btn-group");
+
         // Delete button
         const deleteBtn = document.createElement("button");
         deleteBtn.classList.add("delete");
-        deleteBtn.textContent = "Delete";
+        deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`
         deleteButton(deleteBtn);
 
         // Update read status button
         const readBtn = document.createElement("button");
         readBtn.classList.add("readBtn");
-        readBtn.textContent = "Change Read Status";
+        readBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M300-80q-58 0-99-41t-41-99v-520q0-58 41-99t99-41h500v600q-25 0-42.5 17.5T740-220q0 25 17.5 42.5T800-160v80H300Zm-60-267q14-7 29-10t31-3h20v-440h-20q-25 0-42.5 17.5T240-740v393Zm160-13h320v-440H400v440Zm-160 13v-453 453Zm60 187h373q-6-14-9.5-28.5T660-220q0-16 3-31t10-29H300q-26 0-43 17.5T240-220q0 26 17 43t43 17Z"/></svg>`;
         readButton(readBtn);
 
+        btnGroup.append(deleteBtn, readBtn);
 
-        bookCard.append(bookName, bookAuthor, bookYear, bookPages, readStatus, deleteBtn, readBtn);
+        bookCard.append(bookName, bookAuthor, bookYear, bookPages, readStatus, btnGroup);
         collection.append(bookCard);
     }
 }
@@ -122,7 +127,7 @@ function addButton() {
 
 function deleteButton(deleteBtn) {
     deleteBtn.addEventListener("click", (event) => {
-        const targetId = event.target.parentElement.dataset.id;
+        const targetId = event.target.parentElement.parentElement.dataset.id;
 
         for (const item of myLibrary) {
             if (item.id == targetId) {
@@ -139,7 +144,7 @@ function deleteButton(deleteBtn) {
 
 function readButton(readBtn) {
     readBtn.addEventListener("click", (event) => {
-        const targetId = event.target.parentElement.dataset.id;
+        const targetId = event.target.parentElement.parentElement.dataset.id;
 
         for (const item of myLibrary) {
             if (item.id == targetId) {
